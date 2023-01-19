e8bccb92a5376c9391ac6cc4cc9849c6e064fbe5
Oleg <olegashepelev@gmail.com>
2023-01-19 23:49:20 +0600

    Complete third task

---
+++ 03/Program.cs
@@ -0,0 +1,39 @@
+﻿//
+//Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//Первые два числа Фибоначчи: 0 и 1.
+//
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] CreateArray(int size)
+{
+    int[] res = new int[size];
+    res[0] = 0;
+    res[1] = 1;
+    for (int i = 2; i < size; i++)
+    {
+        res[i] = res[i - 2] + res[i - 1];
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int n = GetNumberFromUser($"Введите число ", "Ошибка ввода!");
+int[] Fib_Array = CreateArray(n);
+PrintArray(Fib_Array);
