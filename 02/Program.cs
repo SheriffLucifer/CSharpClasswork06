b6ea4fa3e81512a65d6e259fa8c4f90bec1629d9
Oleg <olegashepelev@gmail.com>
2023-01-19 23:48:10 +0600

    Complete second task

---
+++ 02/Program.cs
@@ -0,0 +1,41 @@
+﻿//
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// void PrintString(string str)
+// {
+//     for (int i = 0; i < str.Length; i++)
+//     {
+//         Console.Write(str[str.Length - i - 1]);
+
+
+//     }
+// }
+
+string DigitConvert(int x)
+{
+    string s = "";
+    while (x > 0)
+    {
+        if (x % 2 == 1) s = s.Insert(0, "1"); //s=s+"1";
+        else s = s.Insert(0, "0"); //s=s+"0";
+        x = x / 2;
+    }
+    return s;
+}
+
+int n = GetNumberFromUser($"Введите целое число: ", "Ошибка ввода!");
+string row = DigitConvert(n);
+//PrintString(row);
+Console.WriteLine(row);
