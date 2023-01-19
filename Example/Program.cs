715136b6b03ce1782fb8ff64e9c2c75ccd77e8f3
Oleg <olegashepelev@gmail.com>
2023-01-19 23:26:41 +0600

    Complete example task

---
+++ Example/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] reverseArray = ReverseArray1(array);
+Console.WriteLine($"[{String.Join(", ", reverseArray)}]");
+
+ReverseArray2(array);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] ReverseArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+void ReverseArray2(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - i - 1];
+        inArray[inArray.Length - i - 1] = k;
+    }
+}
